09a756eeab8c23333b1fa743f271cd8176739076
Jonathan Davis <cartman61616@gmail.com>
2015-03-26 20:19:37 -0400

    Initial Commit. Using "Learning Python the Hard Way" book

---
+++ ex_3.py
@@ -0,0 +1,36 @@
+print "I will now count my chickens."
+
+# adding dividing 30 by 6 then adding 25
+print "Hens", 25 + 30 / 6
+
+# multiplying 25 times 3 then by 4 percent, then subtracting the answer from 100
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+# finding 2 percent of 4, 1 divided by 4, then adding 3, 2, 1, subtracting five, adding 4 % 2, subtracting 1/4, adding 6   
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7"
+
+#adding 3+2 finding if it is greated than 5-7
+print 3 + 2 < 5 - 7
+
+#adding 3+2 
+print "What is 3 + 2?", 3 + 2
+
+#subtracting 5-7
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's what it's False"
+
+print "How about some more."
+
+#is 5 > -2
+print "Is it greater?", 5 > -2
+
+# is 5 greater than or equal to -2
+print "Is it greater or equal?", 5 >= -2
+
+# is 5 less than or equal to -2
+print "Is it less or equal?", 5 <= -2
---
+++ ex_5.py
@@ -0,0 +1,15 @@
+my_name = 'Jonathan Davis'
+my_age = 26
+my_height = 70 #in inches
+my_weight = 198 #in pounds
+my_eyes = 'Green'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He weighs %d pounds" %  my_weight
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
